a025b059daa52bd33a63ad99e8cd4549f0d272a1
Suhaib Saeed <saeed.suhaib@googlemail.com>
2023-03-11 21:35:33 +0000

    Added sleep for ospf to converge

--- Deploy/deployer.sh
+++ Deploy/deployer.sh
@@ -1,2 +1,3 @@
 python3 ./config_intfs_ospf.py prod
+sleep 10
 python3 -m pytest ./Deploy/validateospf.py --disable-warnings -s --verbose
--- Test/testscript.sh
+++ Test/testscript.sh
@@ -1,2 +1,3 @@
 python3 ./config_intfs_ospf.py test
+sleep 10
 python3 -m pytest ./Test/testospf.py --disable-warnings -s --verbose
